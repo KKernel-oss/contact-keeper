d99e2a99fe4f1f93233938e25b8b167c35960b22
Kevoy Walters <kwalters@gkmsonline.net>
2021-03-09 12:46:53 -0500

    Inital commit

---
+++ Api/Keeper.Models/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Keeper.Models.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+
+    public class LoginDto
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Api/contact-keeper-api/Controllers/KeeperController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keeper.Models.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace contact_keeper_api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class KeeperController : ControllerBase
+    {
+        #region Users
+
+        /// <summary>
+        /// Registers a user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("users")]
+        public IActionResult RegisterUser([FromBody] User model)
+        {
+            try
+            {
+                var resp = new TokenResponse() { token = "1234567890" };
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+        /// <summary>
+        /// Authenticate user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("auth")]
+        public IActionResult Login([FromBody] LoginDto model)
+        {
+            try
+            {
+                var resp = new TokenResponse() { token = "1234567890" };
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+        /// <summary>
+        /// Get a existing user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("auth")]
+        public IActionResult FindUser([FromBody] LoginDto model)
+        {
+            try
+            {
+                var resp = new TokenResponse() { token = "3456" };
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+        #endregion
+
+    }
+}
